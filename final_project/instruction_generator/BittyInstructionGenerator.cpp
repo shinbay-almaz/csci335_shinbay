4c9d47cfd2fd64272fc6d75fc73a1f12edb320e4
Almaz Shinbay <shinbay.almaz@gmail.com>
2024-10-21 11:58:25 +0500

    Lab 15 [Bitty Instruction Generator]: Changed to standalone CLI program

---
+++ final_project/instruction_generator/BittyInstructionGenerator.cpp
@@ -0,0 +1,34 @@
+#include "BittyInstructionGenerator.h"
+#include <random>
+#include <ctime>
+#include <string>
+#include <iostream>
+
+uint16_t BittyInstructionGenerator::Generate() {
+  uint16_t instruction = 0;
+
+  uint16_t rx = rand() & 0x7;
+  uint16_t ry = rand() & 0x7;
+
+  uint16_t alu_sel = rand() & 0x7;
+
+  instruction = (rx << 13) + (ry << 10) + (alu_sel << 2);
+
+  return instruction;
+}
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  int N = stoi(argv[1]);
+
+  BittyInstructionGenerator generator;
+
+  freopen("instructions.txt", "w", stdout);
+
+  for (int i = 0; i < N; i++) {
+    uint16_t instruction = generator.Generate();
+    
+    cout << hex << instruction << endl;
+  }
+}
