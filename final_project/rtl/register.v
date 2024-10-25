98295be9f62d83757f80866e3dbc8473619202cf
Almaz Shinbay <shinbay.almaz@gmail.com>
2024-10-25 20:52:01 +0500

    [Register]: Code Cleanup

--- final_project/rtl/register.v
+++ final_project/rtl/register.v
@@ -1,21 +1,18 @@
 module register (
   input [15:0] d_in,
-  input [15:0] init_val,
   input reset,
   input clk, 
   input enable,
   output reg [15:0] d_out
 );
 
+parameter INIT_VAL = 16'd2;
+
 always @ (posedge clk, posedge reset) begin
   if (reset)
-    d_out <= init_val;
-  else begin
-    if (enable) 
-      d_out <= d_in;
-    else 
-      d_out <= d_out;
-  end
+    d_out <= INIT_VAL;
+  else if (enable)
+    d_out <= d_in;
 end
 
 endmodule
