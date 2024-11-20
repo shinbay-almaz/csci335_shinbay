980142afa71b3687174e79fed8e206d9d272327c
Almaz Shinbay <shinbay.almaz@gmail.com>
2024-11-20 20:52:07 +0500

    Lab 18: A Complete Version of Branch Logic

--- final_project/rtl/branch_logic.v
+++ final_project/rtl/branch_logic.v
@@ -1,26 +1,20 @@
 module branch_logic(
-//  input [15:0] instruction,
+  input [15:0] instruction,
   input [7:0] pc,
-//  input [15:0] last_alu_result,
+  input [15:0] last_alu_result,
   input done,
   output reg [7:0] new_pc
 );
 
 always @ (*) begin
+  new_pc = pc;
   if (done) begin
     new_pc = pc + 1;
-  end else new_pc = pc;
-  /*
-  if (instruction[1:0] == 2'd2) begin
-    if ({14'd0, instruction[3:2]} == last_alu_result) begin
-      new_pc = instruction[11:4];
-    end else begin
-      new_pc = pc + 1;
+    if (instruction[1:0] == 2'd2) begin
+      if (instruction[3:2] == last_alu_result[1:0]) 
+        new_pc = instruction[11:4];
     end
-  end else begin
-    new_pc = pc + 1;
   end
-  */
 end
 
 endmodule
--- final_project/test/branch_logic/branch_logic_tb.cpp
+++ final_project/test/branch_logic/branch_logic_tb.cpp
@@ -2,6 +2,8 @@
 #include "verilated.h"
 
 #include <iostream>
+#include <random>
+#include <vector>
 
 using namespace std;
 
@@ -12,17 +14,32 @@ int main(int argc, char** argv) {
   // Set input signals of Branch Logic to default
   bl->pc = 0;
   bl->done = 0;
+  bl->instruction = 0;
+  bl->last_alu_result = 0;
   bl->eval();
 
-  for (int i = 0; i < 255; i++) {
+  while ((int)bl->pc != 0xFF) {
+    int jump_address = rand() & 0xFF;
+    int cond = rand() % 3;
+    int last_alu_result = rand() % 3;
+
     bl->done = 1;
+    bl->instruction = (jump_address << 4) + (cond << 2) + 2;
+    bl->last_alu_result = last_alu_result;
     bl->eval();
 
-    int next_pc = (int)bl->new_pc;
-  
-    cout << (int)bl->pc << " " << (int)bl->new_pc << endl;
+    int expected_next_pc = (int)bl->pc + 1;
+    if (cond == last_alu_result) {
+      cout << "Making a jump to address 0x" << hex << jump_address << endl;
+      expected_next_pc = jump_address;
+    } else {
+      cout << "Moving to the next address 0x" << hex << (int)bl->pc + 1 << endl;
+    }
 
-    assert(next_pc == (int)bl->pc + 1);
+    int next_pc = (int)bl->new_pc;
+    cout << expected_next_pc << " " << (int)bl->new_pc << endl;
+
+    assert(next_pc == expected_next_pc);
 
     bl->pc = next_pc;
   }
@@ -30,5 +47,3 @@ int main(int argc, char** argv) {
   delete bl;
   return 0;
 }
-
-
